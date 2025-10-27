3a2e2ea3f488300c2e841a8570ed92f0f4669fcd
Kimheang <kimheang135@gmail.com>
2025-10-27 13:30:28 +0700

    set the default view logger for tmp

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Opcodes\LogViewer\Facades\LogViewer;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +22,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         URL::forceHttps(app()->isProduction());
+
+        LogViewer::auth(function ($request) {
+            return true; // TODO: Implement proper authentication
+        });
     }
 }
