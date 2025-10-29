658a90a5e09a01af2c557380c542ef989ff45801
Kimheang <kimheang135@gmail.com>
2025-10-29 15:02:33 +0700

    add IP config

---
+++ config/security.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Allowed Admin IPs
+    |--------------------------------------------------------------------------
+    |
+    | A list of comma-separated IP addresses that are allowed to access
+    | the admin panel. You can use '*' to allow all IPs.
+    |
+    */
+    'allowed_ips' => env('APP_ALLOWED_IPS', '*'),
+];
