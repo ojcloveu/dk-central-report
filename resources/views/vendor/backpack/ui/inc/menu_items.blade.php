68c0966e27186e81060e910ff0863143895d00b4
Kimheang <kimheang135@gmail.com>
2025-10-29 14:19:25 +0700

    update menu

--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -1,6 +1,13 @@
-{{-- This file is used for menu items by any Backpack v6 theme --}}
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i>
-        {{ trans('backpack::base.dashboard') }}</a></li>
+<x-backpack::menu-item title="{{ trans('backpack::base.dashboard') }}" icon='la la-home' :link="backpack_url('dashboard')" />
+
+<x-backpack::menu-item title="Bets" icon="la la-dice" :link="backpack_url('bet')" />
+
+<x-backpack::menu-dropdown title="User Management" icon="la la-user">
+    <x-backpack::menu-dropdown-header title="User Management" />
+    <x-backpack::menu-dropdown-item title="Users" icon="la la-user" :link="backpack_url('user')" />
+    <x-backpack::menu-dropdown-item title="Roles" icon="la la-group" :link="backpack_url('role')" />
+    <x-backpack::menu-dropdown-item title="Permissions" icon="la la-key" :link="backpack_url('permission')" />
+</x-backpack::menu-dropdown>
 
 <x-backpack::menu-item title='Settings' icon='la la-cog' :link="backpack_url('setting')" />
 
@@ -10,5 +17,3 @@
         <x-backpack::menu-dropdown-item title="Log Viewers" icon='la la-clock' :link="backpack_url('log-viewer')" />
     </x-theme-tabler::menu-dropdown-column>
 </x-backpack::menu-dropdown>
-
-<x-backpack::menu-item title="Bets" icon="la la-question" :link="backpack_url('bet')" />
