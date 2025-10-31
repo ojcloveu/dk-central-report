990822a9a116f7652d5370d4c96c55ff54948f68
Rotha SAMON <samonrotha@gmail.com>
2025-10-31 14:59:58 +0700

    rename the pages to page folder

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,8 +8,8 @@ const pinia = createPinia();
 
 createInertiaApp({
   resolve: name => {
-    const pages = import.meta.glob('./pages/**/*.vue', { eager: true })
-    return pages[`./pages/${name}.vue`]
+    const pages = import.meta.glob('./page/**/*.vue', { eager: true })
+    return pages[`./page/${name}.vue`]
   },
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
