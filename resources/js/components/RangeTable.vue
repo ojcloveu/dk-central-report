24c815bc6ac9a78fa0bb73e73bdf3902e7c52ad6
Rotha SAMON <samonrotha@gmail.com>
2025-10-31 11:50:30 +0700

    add option for page

--- resources/js/components/RangeTable.vue
+++ resources/js/components/RangeTable.vue
@@ -60,9 +60,10 @@ const handleChangePerPage = (rangeKey, count) => {
                             :value="getRangeData(period.key).meta?.per_page"
                             @change="handleChangePerPage(period?.key, $event.target?.value)"
                         >
-                            <option value="5">5</option>
                             <option value="10">10</option>
                             <option value="20">20</option>
+                            <option value="50">50</option>
+                            <option value="100">100</option>
                         </select>
                     </div>
                 </div>
