04c3c3ca0869fb1f3b050821a38274111fee0059
Rotha SAMON <samonrotha@gmail.com>
2025-11-03 11:59:56 +0700

    using fetchBetsWithReset store in the components

--- resources/js/components/BetPageHeader.vue
+++ resources/js/components/BetPageHeader.vue
@@ -1,3 +1,4 @@
+<!-- BetPageHeader.vue -->
 <script setup>
 const props = defineProps({
     onRefresh: Function,
@@ -14,7 +15,8 @@ const props = defineProps({
 
             <div class="col-auto ms-auto">
                 <div class="btn-list">
-                    <button class="btn btn-icon" aria-label="Refresh" @click="onRefresh">
+                    <!-- Refresh table button -->
+                    <button class="btn btn-icon btn-primary" aria-label="Refresh" @click="onRefresh">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             width="24"
@@ -31,34 +33,6 @@ const props = defineProps({
                             <path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4"></path>
                         </svg>
                     </button>
-
-                    <div class="dropdown">
-                        <button class="btn dropdown-toggle" data-bs-toggle="dropdown">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                stroke-width="2"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                class="icon me-1"
-                            >
-                                <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2"></path>
-                                <path d="M7 11l5 5l5 -5"></path>
-                                <path d="M12 4l0 12"></path>
-                            </svg>
-                            Export
-                        </button>
-
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="#">Export as CSV</a>
-                            <a class="dropdown-item" href="#">Export as Excel</a>
-                            <a class="dropdown-item" href="#">Export as PDF</a>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
