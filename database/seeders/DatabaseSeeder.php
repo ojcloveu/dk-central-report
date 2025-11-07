7357394c5b07c7f492ccc4abd513f263d2833f49
Rotha SAMON <samonrotha@gmail.com>
2025-11-07 10:53:51 +0700

    Remove the test user in seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,18 +13,11 @@ class DatabaseSeeder extends Seeder
 
     /**
      * Seed the application's database.
-     * 
-     * php artisan db:seed --class="Database\\Seeders\\DatabaseSeeder"
+     *
+     * php artisan db:seed
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-
         $this->call([
             DefaultUserSeeder::class,
             ChannelSeeder::class,
