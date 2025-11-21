62b5512116a2476105b66a64a0233e2dccbba920
Rotha SAMON <samonrotha@gmail.com>
2025-11-21 09:51:20 +0700

    Make a component ActionButton

---
+++ resources/js/components/buttons/ActionButton.vue
@@ -0,0 +1,50 @@
+<script setup>
+defineProps({
+    variant: {
+        type: String,
+        default: 'primary',
+        validator: value =>
+            ['primary', 'secondary', 'outline-secondary', 'danger', 'success'].includes(value),
+    },
+    iconClass: {
+        type: String,
+        required: true,
+    },
+    label: {
+        type: String,
+        default: '',
+    },
+    title: {
+        type: String,
+        default: '',
+    },
+    disabled: {
+        type: Boolean,
+        default: false,
+    },
+    iconOnly: {
+        type: Boolean,
+        default: false,
+    },
+    loading: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+defineEmits(['click']);
+</script>
+
+<template>
+    <button
+        type="button"
+        :class="['btn', iconOnly ? 'btn-icon' : 'px-2', `btn-${variant}`]"
+        :title="title"
+        :disabled="disabled"
+        :aria-label="iconOnly ? title : undefined"
+        @click="$emit('click')"
+    >
+        <i :class="[iconClass, 'fs-2', { 'fa-spin': loading }, iconOnly ? '' : 'pe-1']"></i>
+        <span v-if="!iconOnly">{{ label }}</span>
+    </button>
+</template>
