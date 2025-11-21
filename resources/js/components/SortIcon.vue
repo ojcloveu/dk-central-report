ea78a5687d231e50a146ffe4eb1f23245f616676
Rotha SAMON <samonrotha@gmail.com>
2025-11-21 09:40:22 +0700

    Make a SortIcon component

---
+++ resources/js/components/SortIcon.vue
@@ -0,0 +1,32 @@
+<script setup>
+defineProps({
+    currentSortBy: {
+        type: String,
+        required: true,
+    },
+    columnName: {
+        type: String,
+        required: true,
+    },
+    sortDirection: {
+        type: String,
+        required: true,
+        validator: value => ['asc', 'desc'].includes(value),
+    },
+});
+</script>
+
+<template>
+    <i
+        v-if="currentSortBy === columnName"
+        class="las la-sort-amount-down fs-4"
+        :class="{ 'rotate-180': sortDirection === 'desc' }"
+    ></i>
+</template>
+
+<style scoped>
+.rotate-180 {
+    transform: rotate(180deg);
+    display: inline-block;
+}
+</style>
