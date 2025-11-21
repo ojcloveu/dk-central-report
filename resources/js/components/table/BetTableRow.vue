0e5b07a8eb23c33d52d495c3ab5201685288b44f
Rotha SAMON <samonrotha@gmail.com>
2025-11-21 13:44:53 +0700

    Remove fix

--- resources/js/components/table/BetTableRow.vue
+++ resources/js/components/table/BetTableRow.vue
@@ -62,7 +62,7 @@ const handleCheckboxChange = event => {
             {{ bet?.winlose }}
         </td>
         <td class="text-start fw-bold" :class="lpBgColor(bet?.lp)">
-            {{ (parseFloat(bet?.lp?.percentage) * 100).toFixed(2) }}%
+            {{ (parseFloat(bet?.lp?.percentage) * 100) }}%
         </td>
     </tr>
 </template>
