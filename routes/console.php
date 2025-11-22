3a90d5cd08b07472534d8b890de1707abef43cc6
Rony Por <ojcloveu@yahoo.com>
2025-11-22 10:10:10 +0700

    enable schedule every 5mn

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Schedule bet sync command to run every 5 minutes without overlapping
+Schedule::command('sync:bets --days-back=0')
+    ->everyFiveMinutes()
+    ->withoutOverlapping()
+    ->runInBackground();
