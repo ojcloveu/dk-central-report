8520fcf65838af46301eedce7e987375d1616b46
Rony Por <ojcloveu@yahoo.com>
2025-11-23 21:01:28 +0700

    add uniquic

---
+++ database/migrations/2025_11_23_205931_add_unique_constraint_to_bets_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('bets', function (Blueprint $table) {
+            $table->unique(['account', 'channel', 'trandate'], 'uq_bets_acc_chan_trandate');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('bets', function (Blueprint $table) {
+            $table->dropUnique('uq_bets_acc_chan_trandate');
+        });
+    }
+};
